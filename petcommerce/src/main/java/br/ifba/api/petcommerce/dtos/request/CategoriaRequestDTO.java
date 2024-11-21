dfd42f376e6411c97cd6f06086214313d8d72087
Pedro Henrique Barreto Velloso <pedrohenriquevelloso18@gmail.com>
2024-11-21 20:04:42 -0300

    Adiciona CategoriaRequestDTO

---
+++ petcommerce/src/main/java/br/ifba/api/petcommerce/dtos/request/CategoriaRequestDTO.java
@@ -0,0 +1,13 @@
+package br.ifba.api.petcommerce.dtos.request;
+
+public class CategoriaRequestDTO {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
