a9519a46ec369d268e04f80a49f15aca55b83251
Pedro Henrique Barreto Velloso <pedrohenriquevelloso18@gmail.com>
2024-11-21 19:56:56 -0300

    Retira repositorio de endereço uma vez que é associado ao cliente

--- petcommerce/src/main/java/br/ifba/api/petcommerce/repositories/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.ifba.api.petcommerce.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.ifba.api.petcommerce.entities.Endereco;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
-
-}
